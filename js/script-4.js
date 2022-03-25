aa667a9199428ae03253b267ef0d9e9ee2be7240
Anton <anton_snov@mail.ru>
2022-03-25 09:55:22 +0300

    5 practice

--- js/script-4.js
+++ js/script-4.js
@@ -35,32 +35,32 @@ const personalMovieDB = {
     privat: false
 };
 
-// function rememberMyFilms() {
-//     for (let i=0; i<2; i++) {
-//         let a = prompt('Один из последних просмотренных фильмов?', ''),
-//             b = prompt('На сколько оцените его?', "");
-//         if ( a != null && b != null && a != '' && b != '' && a.length <50 ) {
-//             personalMovieDB.movies[a] = b;
-//             console.log('done');
-//         } else {
-//             i--;
-//         }
-//     }
-// }
-// rememberMyFilms();
+function rememberMyFilms() {
+    for (let i=0; i<2; i++) {
+        let a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', "");
+        if ( a != null && b != null && a != '' && b != '' && a.length <50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            i--;
+        }
+    }
+}
+rememberMyFilms();
 
-// function detectPersonalLevel () {
-//     if (personalMovieDB.count < 10) {
-//         console.log("Просмотрено довольно мало фильмов");
-//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
-//         console.log("Вы классический зритель");
-//     } else if (personalMovieDB.count >= 30) {
-//         console.log("Вы киноман"); 
-//     } else {
-//         console.log("Произошла ошибка");
-//     }
-// }
-// detectPersonalLevel ();
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман"); 
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectPersonalLevel ();
 
 function showMyDB(hidden) {
     if (!hidden) {
--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-
-function random(min, max) {
-            return Math.ceil(min + Math.random()*(max-min));
-}
-console.log(random(1, 100));
